c2d5558ae61a88c0c89d925a90b4d0fa87f92cde
Jeremy Gil <hplaya@gmail.com>
2015-07-25 18:29:02 +0900

    add SpringApplication config.

---
+++ src/main/java/org/remyg/WebApp.java
@@ -0,0 +1,9 @@
+package org.remyg;
+
+import org.springframework.boot.SpringApplication;
+
+public class WebApp {
+  public static void main(String args[]) {
+    SpringApplication.run(WebAppConfig.class);
+  }
+}
