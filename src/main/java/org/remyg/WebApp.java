db6199285809f4b0a8440260b4acd308e5f1ee5a
Remy Gil <hplaya@gmail.com>
2015-08-24 00:52:59 +0900

    rearrange build.gradle .

--- src/main/java/org/remyg/WebApp.java
+++ src/main/java/org/remyg/WebApp.java
@@ -1,16 +1,11 @@
 package org.remyg;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 public class WebApp {
 
   public static void main(String args[]) {
     SpringApplication.run(WebAppConfig.class);
-  }
-
-  @EnableAutoConfiguration
-  public class WebAppConfig {
   }
 
 }
---
+++ src/main/java/org/remyg/WebAppConfig.java
@@ -0,0 +1,7 @@
+package org.remyg;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class WebAppConfig {
+}
